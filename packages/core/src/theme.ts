ac4bfc47b4ddbd5aedfba268c003c85bbc581b2e
ylfjuk <ylfjuk@gmail.com>
2024-12-21 09:13:41 +0200

    chore(ui/core): changed order of themes

--- packages/core/src/theme.ts
+++ packages/core/src/theme.ts
@@ -1,9 +1,9 @@
 import type { ValueOf } from '@ylfjuk-core/types';
 
 export const Theme = {
+    System: 'system',
     Light: 'light',
     Dark: 'dark',
-    System: 'system',
 } as const;
 
 export type Theme = ValueOf<typeof Theme>;
